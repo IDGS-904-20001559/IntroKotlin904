391b0a2f1f41a7e96d7bd4c0f1a12aa507cd18a0
IDGS-904-20001559 <20001559@alumnos.utleon.edu.mx>
2025-05-06 19:26:14 -0600

    Proyecto

---
+++ app/src/main/java/com/example/introkotin904/TemaKotlin/Tema1Variables.kt
@@ -0,0 +1,21 @@
+package com.example.introkotin904.TemaKotlin
+
+fun main(){
+    //VARIABLES
+    // VAR: variable mutable
+    // VAL: variable de lectura
+
+    val a = 4
+    val b = 8
+    val c = 10
+
+    println(a)
+    println("El valor de la b es: $b")
+    println("El valor de la c + 2 es: ${c+2}")
+
+    //val num1 : int=23
+    //val num2 : int=12
+
+
+
+}
---
+++ app/src/main/java/com/example/introkotin904/TemaKotlin/tema2Collections.kt
@@ -0,0 +1,28 @@
+package com.example.introkotin904.TemaKotlin
+
+
+fun main(){
+
+
+
+
+    /*
+    var figuras : mutableFiguras = mutableListOf("cuadrado", "triangulo", "circulo")
+    val readOnlyfiguras = listOf("cuadrado", "triangulo", "circulo")
+    val mutableFiguras  : list<String> = figuras
+    */
+
+    /*
+    val frutas = setOf("manzana", "banana", "naranja")
+    val frutas = mutableSetOf("manzana", "banana", "naranja")
+    */
+
+    /*
+    val coches = mapOf("uno" to 1, "dos" to 2, "tres" to 3)
+    println(coches)
+    val coches2 = mutableMapOf("uno" to 1, "dos" to 2, "tres" to 3)
+    println(coches2)
+    */
+
+
+}
