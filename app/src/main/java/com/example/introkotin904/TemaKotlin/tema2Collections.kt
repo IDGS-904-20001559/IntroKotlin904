e07f1b92a31a5476123041b80f48963aee1cce6a
IDGS-904-20001559 <20001559@alumnos.utleon.edu.mx>
2025-05-06 19:36:40 -0600

    tema 3 if when

--- app/src/main/java/com/example/introkotin904/TemaKotlin/tema2Collections.kt
+++ app/src/main/java/com/example/introkotin904/TemaKotlin/tema2Collections.kt
@@ -25,4 +25,14 @@ fun main(){
     */
 
 
+    val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
+    println(readOnlyFiguras)
+    println(" la primera figura es ${readOnlyFiguras[0]}")
+    println("el primer elemento es ${readOnlyFiguras.first()}")
+    println("numero de elementos ${readOnlyFiguras.count()} items")
+    println("circulo" in readOnlyFiguras)
+    //readOnlyFiguras.add("pentagono")
+    var figura : MutableList<String> = mutableListOf("cuadrado2","triangulo2","circulo2")
+    figura.add("pentagono2")
+    figura.remove("cuadrado2")
 }
